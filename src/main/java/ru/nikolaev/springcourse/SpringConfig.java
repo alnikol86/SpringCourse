d086bb7fb466ae2f4c7f50ffa97abe920a7807e5
Alexander Nikolaev <nikolaevairman@gmail.com>
2021-06-28 22:34:37 +0300

    Spring configuration with Java code

---
+++ src/main/java/ru/nikolaev/springcourse/SpringConfig.java
@@ -0,0 +1,11 @@
+package ru.nikolaev.springcourse;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.nikolaev.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+}
--- src/main/java/ru/nikolaev/springcourse/TestSpring.java
+++ src/main/java/ru/nikolaev/springcourse/TestSpring.java
@@ -1,12 +1,11 @@
 package ru.nikolaev.springcourse;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringConfig.class);
 
 //        Computer computer = context.getBean("computer", Computer.class);
 //        System.out.println(computer);
